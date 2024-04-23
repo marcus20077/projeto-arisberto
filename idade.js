270ad7742ff16c7184b82501f5a03f778161f948
marcus20077 <129951348+marcus20077@users.noreply.github.com>
2024-04-23 08:34:17 -0300

    Create idade.js

---
+++ idade.js
@@ -0,0 +1,10 @@
+const idade = 15;
+if(idade >= 18){
+
+console.log("Sua idade é: " + idade + ",você é maior de idade");
+
+
+
+}else{
+    console.log("Sua idade é: " + idade + ",você é menor de idade");
+}
