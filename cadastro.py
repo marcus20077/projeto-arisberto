0474085ef551d941287c3fb81f218d37c0e223bd
marcus20077 <129951348+marcus20077@users.noreply.github.com>
2024-04-24 09:11:47 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,26 @@
+nome = input ("digite seu nome");
+idade = input ("digite sua idade");
+sexo = input ("digite seu sexo");
+
+nome2 = input ("digite seu nome");
+idade2 = input ("digite sua idade");
+sexo2 = input ("digite seu sexo");
+
+nome3 = input ("digite seu nome");
+idade3 = input ("digite sua idade");
+sexo3 = input ("digite seu sexo");
+
+
+print ("nome:", nome)
+print ("idade:", idade)
+print ("sexo:", sexo)
+
+
+print ("nome2:", nome)
+print ("idade2:", idade)
+print ("sexo2:", sexo)
+
+
+print ("nome3:", nome)
+print ("idade3:", idade)
+print ("sexo3:", sexo)
