db26b953288814ce45a0837ae86bbf41cfe9171f
marcus20077 <129951348+marcus20077@users.noreply.github.com>
2024-05-07 09:34:46 -0300

    Create tabuada.js

---
+++ tabuada.js
@@ -0,0 +1,54 @@
+console.log("1x1 = 1");
+console.log("1x2 = 2");
+console.log("1x3 = 3");
+console.log("1x4 = 4");
+console.log("1x5 = 5");
+console.log("1x6 = 6");
+console.log("1x7 = 7");
+console.log("1x8 = 8");
+console.log("1x9 = 9");
+console.log("1x10 = 10");
+
+
+
+console.log("3x1 = 3");
+console.log("3x2 = 6");
+console.log("3x3 = 9");
+console.log("3x4 = 12");
+console.log("3x5 = 15");
+console.log("3x6 = 18");
+console.log("3x7 = 21");
+console.log("3x8 = 24");
+console.log("3x9 = 27");
+console.log("3x10 = 30");
+
+console.log("7x1 = 7");
+console.log("7x2 = 14");
+console.log("7x3 = 21");
+console.log("7x4 = 28");
+console.log("7x5 = 35");
+console.log("7x6 = 42");
+console.log("7x7 = 49");
+console.log("7x8 = 56");
+console.log("7x9 = 63");
+console.log("7x10 = 70");
+
+
+const t=1;
+console.log(t+" x 1 =", t*1)
+console.log(t+" x 2 =", t*2)
+console.log(t+" x 3 =", t*3)
+console.log(t+" x 4 =", t*4)
+console.log(t+" x 5 =", t*5)
+console.log(t+" x 6 =", t*6)
+console.log(t+" x 7 =", t*7)
+console.log(t+" x 8 =", t*8)
+console.log(t+" x 9 =", t*9)
+console.log(t+" x 10 =", t*10)
+
+lett = 64;
+console.log (t+"x 1 =", t*1);
+t = t + 1;
+console.log(t+"x 1 =", t*1);
+t++;
+console.log(t + "x1=", t*1);
