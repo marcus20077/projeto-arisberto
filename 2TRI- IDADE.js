494595bf297347348ef7a80ad8405a80d7d4def4
marcus reis <129951348+marcus20077@users.noreply.github.com>
2024-05-28 09:41:53 -0300

    Create 2TRI- IDADE.js

---
+++ 2TRI- IDADE.js
@@ -0,0 +1,19 @@
+console.log (soma (5,9));
+function soma (v1=0, v2=0){
+    return v1 + v2;
+
+}
+function nomeidade(nome="", idade=""){
+    console.log (`nome: ${nome}-idade: ${idade}`);
+   } 
+nomeidade("marcus "," 16")
+  
+nomeidade("16","marcus")
+ 
+console.log(soma(1));
+nomeidade("vinicius");
+
+console.log (multiplicação (5,9));
+function multiplicação (v1, v2){
+    return v1*v2;
+}
