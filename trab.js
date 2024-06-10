5fc1ceb840c051022a711288101d49ec0ddbbde1
marcus reis <129951348+marcus20077@users.noreply.github.com>
2024-06-10 12:34:57 -0300

    Create trab.js

---
+++ trab.js
@@ -0,0 +1,13 @@
+const lista = [ ]
+lista.push(" ")
+lista PopStateEvent("feijão", "arroz", "carne", "bombril", "fosforo", "gordura", "banha", "frutas", "maminha")
+nlist = lista.splice (3,4)
+console.log(nlista)
+lista1 = lista.slice(0,5)
+lista2 = lista.slice(5)
+console.log(lista1)
+console.log(lista2)
+lista1.push(" ")
+lista2.push(" ")
+listacompleta = lista1.concat(lista2)
+console.log(listacompleta) 
