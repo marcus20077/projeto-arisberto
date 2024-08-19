2fd928398f4e7b2abfd6c50961c40c3f80e23a41
marcus reis <129951348+marcus20077@users.noreply.github.com>
2024-08-19 12:25:14 -0300

    Create array_lista.py

---
+++ array_lista.py
@@ -0,0 +1,6 @@
+const list = ['laranja','morango','uva','maça','banana'];
+const frutasMaisculas = [];
+for (let i = 0; i<list.length;i++){
+    frutasMaisculas.push(list[i].toUpperCase());
+}
+console.log(frutasMaisculas);
